aba00575d85026cde2c60a23b2c9ab957e4f42d8
Vincent Chho <vincec362@gmail.com>
2018-08-16 20:50:46 -0700

    day2 pushes

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  authUser: object = {
+    email: '',
+    password: ''
+  };
+
+  constructor() { }
+
+  login(){
+    console.log(this.authUser);
+  }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/modules/app-routing/app-routing.module.ts
+++ src/app/modules/app-routing/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
 
-import { HomeComponent } from "../../component/home/home.component";
+import { HomeComponent } from "../../components/home/home.component";
+import { LoginComponent } from '../../components/login/login.component';
+import { RegisterComponent } from "../../components/register/register.component";
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
+
 ]
 
 @NgModule({
